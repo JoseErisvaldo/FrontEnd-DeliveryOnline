6fdfc50fa35b369b41ae4f62a096eb829b7538cd
JoseErisvaldo <joseerisvaldopg@gmail.com>
2025-09-18 23:10:16 -0300

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import './App.css';
-import { Button } from './components/ui/button';
+import { RouterApp } from './router-app';
 
 function App() {
-
   return (
-    <>
-      <div className="bg-amber-900">
-        <h1>Vite + React</h1>
-        <Button>Click me</Button>
-      </div>
-    </>
+    <div className="">
+      <RouterApp />
+    </div>
   );
 }
 
