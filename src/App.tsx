258c92a25107b775445ea4aa0b7b2e3e61c4df3b
JoseErisvaldo <joseerisvaldopg@gmail.com>
2025-09-18 22:47:33 -0300

    feat: add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import { Button } from './components/ui/button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className='bg-amber-900' ><h1>Vite + React</h1></div>
+      <div className="bg-amber-900">
+        <h1>Vite + React</h1>
+        <Button>Click me</Button>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
