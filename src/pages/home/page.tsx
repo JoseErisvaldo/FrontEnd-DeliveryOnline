cb185d3e44ed6b97a16c33a7d13eedcbc98ccf9d
JoseErisvaldo <joseerisvaldopg@gmail.com>
2025-09-19 01:53:47 -0300

    feat: add buttom signOut

--- src/pages/home/page.tsx
+++ src/pages/home/page.tsx
@@ -1,8 +1,13 @@
+import { useAuth } from "@/components/context/auth-context";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
+  const { signOut } = useAuth();
   return (
     <div>
       <h1>Bem Vindo</h1>
       <div>Home</div>
+      <Button variant="destructive" onClick={signOut}>Sair</Button>
     </div>
   );
 }
