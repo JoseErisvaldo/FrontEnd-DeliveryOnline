3408413e32a13906bbcadbb09766f404f7f0413d
JoseErisvaldo <joseerisvaldopg@gmail.com>
2025-09-21 02:21:54 -0300

    feat: add type alert in Establishment

--- src/components/settingsSettings/store.tsx
+++ src/components/settingsSettings/store.tsx
@@ -1,53 +1,85 @@
-import { useGetAllEstablishments } from "@/hooks/establishment/use-get-all-establishment";
-import { Card, CardHeader, CardContent } from "../ui/card";
-import { MapPin, IdCard, CalendarRange, House } from "lucide-react";
-import { Alerts } from "../common/alert";
-import { SkeletonEstablishment } from "../common/skeleton/skeleton-establishment";
+import { useGetAllEstablishments } from '@/hooks/establishment/use-get-all-establishment';
+import { Card, CardHeader, CardContent } from '../ui/card';
+import { MapPin, IdCard, CalendarRange, House } from 'lucide-react';
+import { Alerts } from '../common/alert';
+import { SkeletonEstablishment } from '../common/skeleton/skeleton-establishment';
 
 export default function Store() {
   const { establishments, loading, error } = useGetAllEstablishments();
   if (loading) return <SkeletonEstablishment />;
-  if (error) return <Alerts message={{ title: "Erro ao buscar os estabelecimentos", description: error }} type="destructive" />;
-  if (establishments.length === 0) return <Alerts message={{ title: "Nenhum estabelecimento encontrado", description: "Por favor, adicione um estabelecimento para continuar." }} />;
+  if (error)
+    return (
+      <Alerts
+        message={{
+          title: 'Erro ao buscar os estabelecimentos',
+          description: error,
+        }}
+        type="destructive"
+      />
+    );
+  if (establishments.length === 0)
+    return (
+      <Alerts
+        message={{
+          title: 'Nenhum estabelecimento encontrado',
+          description: 'Por favor, adicione um estabelecimento para continuar.',
+        }}
+        type="destructive"
+      />
+    );
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
-      {establishments.map((est) => (
-        <Card key={est.id} className="hover:shadow-lg transition-shadow duration-200">
-          <CardHeader>
-            <div className="flex flex-col">
-              <h2 className="font-bold text-lg flex gap-2 items-center"><House className="w-4 h-4" />{est.name}</h2>
-              <h4 className="flex items-center gap-1 text-gray-500">
-                <IdCard className="w-4 h-4" /> Id usuário: {est.ownerId}
-              </h4>
-              <h4 className="flex items-center gap-1 text-gray-500">
-                <IdCard className="w-4 h-4" /> Id empresa: {est.ownerId}
-              </h4>
-            </div>
-          </CardHeader> 
-          
-          <CardContent className="space-y-2 text-sm">
-            <div className="flex items-center gap-2">
-              <CalendarRange className="w-4 h-4" />
-              <span className="font-semibold">Criado:</span>
-              <span>{est.createdAt.toLocaleString()}</span>
-            </div>
-          </CardContent>
-          <CardContent className="space-y-2 text-sm">
-            <div className="flex items-center gap-2">
-              <CalendarRange className="w-4 h-4" />
-              <span className="font-semibold">Atualizado:</span>
-              <span>{est.updatedAt.toLocaleString()}</span>
-            </div>
-          </CardContent>
-         <CardContent className="space-y-2 text-sm">
-          <div className="flex items-center gap-2">
-            <MapPin className="w-4 h-4 text-gray-400" />
-            <span className="font-semibold">Endereço:</span>
-            <span>{est.address}</span>
-          </div>
-        </CardContent>
-        </Card>
-      ))}
-    </div>
+    <>
+      <h2 className="font-bold text-lg mb-2">
+        Configurações{' '}
+        {establishments.length > 1
+          ? 'dos Estabelecimentos'
+          : 'do Estabelecimento'}
+      </h2>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        {establishments.map((est) => (
+          <Card
+            key={est.id}
+            className="hover:shadow-lg transition-shadow duration-200"
+          >
+            <CardHeader>
+              <div className="flex flex-col">
+                <h2 className="font-bold text-lg flex gap-2 items-center">
+                  <House className="w-4 h-4" />
+                  {est.name}
+                </h2>
+                <h4 className="flex items-center gap-1 text-gray-500">
+                  <IdCard className="w-4 h-4" /> Id usuário: {est.ownerId}
+                </h4>
+                <h4 className="flex items-center gap-1 text-gray-500">
+                  <IdCard className="w-4 h-4" /> Id empresa: {est.ownerId}
+                </h4>
+              </div>
+            </CardHeader>
+
+            <CardContent className="space-y-2 text-sm">
+              <div className="flex items-center gap-2">
+                <CalendarRange className="w-4 h-4" />
+                <span className="font-semibold">Criado:</span>
+                <span>{est.createdAt.toLocaleString()}</span>
+              </div>
+            </CardContent>
+            <CardContent className="space-y-2 text-sm">
+              <div className="flex items-center gap-2">
+                <CalendarRange className="w-4 h-4" />
+                <span className="font-semibold">Atualizado:</span>
+                <span>{est.updatedAt.toLocaleString()}</span>
+              </div>
+            </CardContent>
+            <CardContent className="space-y-2 text-sm">
+              <div className="flex items-center gap-2">
+                <MapPin className="w-4 h-4 text-gray-400" />
+                <span className="font-semibold">Endereço:</span>
+                <span>{est.address}</span>
+              </div>
+            </CardContent>
+          </Card>
+        ))}
+      </div>
+    </>
   );
 }
